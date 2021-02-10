a8b736ba390c3df7af787f6ac305f0a83a321d4a
momoka-higashiyama <momoka@comas>
2021-02-10 16:32:41 +0900

    プロジェクト ファイルを追加します。

---
+++ MailYoujin/MainForm.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace MailYoujin
+{
+    public partial class MainForm : Form
+    {
+        private bool isExtDomainBulkCheckEvent  = true;
+        private bool isAttachmentBulkCheckEvent = true;
+
+        /// <summary>
+        /// MainFormの初期化処理
+        /// </summary>
+        /// <param name="item">送信メールのアイテム</param>
+        public MainForm(object item)
+        {
+            InitializeComponent();
+
+            //受信アドレスの判定
+            SendAdressCheck(item);
+
+            if(ExternalDomainCheckBox.Items.Count == 0)
+            {
+                ExternalDomainBulkCheckBox.Enabled = false;
+            }
+
+            //添付ファイルの判定
+            AttachmentCheck(item);
+            
+            if (AttachmentCheckBox.Items.Count == 0)
+            {
+                AttachmentBulkCheckBox.Enabled = false;
+            }
+
+            //送信ボタンの活性非活性確認
+            SendButtonEnableCheck();
+        }
+
+
+        /// <summary>
+        /// アドレスの判定
+        /// </summary>
+        /// <param name="item">送信メールのアイテム</param>
+        private void SendAdressCheck(object item)
+        {
+
+            //メールアドレスをSMTPで取得
+            Outlook.MailItem   mailItem     = item as Outlook.MailItem;
+            Outlook.Recipients recipients   = mailItem.Recipients;
+
+            int     internalDomainCount     = 0;
+            int     externalDomainCount     = 0;
+            string　strIntDomain            = ConfigurationManager.AppSettings["internalDomain"];
+            string  recipientType;
+
+
+            const string PR_SMTP_ADDRESS =
+                  "http://schemas.microsoft.com/mapi/proptag/0x39FE001E";
+
+            foreach (Outlook.Recipient recipient in recipients)
+            {
+                //受信者のアドレスを取得。
+                    //Outlookに名前が登録されている場合、名前しか取得できない。
+                    //SMTPはメール送信サーバのプロトコル。
+                Outlook.PropertyAccessor propertyAccessor = recipient.PropertyAccessor;
+                string smtpAddress = propertyAccessor.GetProperty(PR_SMTP_ADDRESS).ToString();
+
+
+                //宛先判定
+                if ((int)Outlook.OlMailRecipientType.olTo == recipient.Type)
+                {
+                    recipientType = "(TO：)";
+                }
+                else if ((int) Outlook.OlMailRecipientType.olCC == recipient.Type)
+                {
+                    recipientType = "(CC：)";
+                }
+                else if ((int) Outlook.OlMailRecipientType.olBCC == recipient.Type)
+                {
+                    recipientType = "(BCC：)";
+                }
+                else
+                {
+                    recipientType = String.Empty;
+                }
+
+                bool isIntDomain = smtpAddress.Contains(strIntDomain); 
+
+                //社内ドメイン
+                if (isIntDomain == true)
+                {
+                    //Outlookに名前が登録されているかの判定
+                    if (recipient.Name.Equals(smtpAddress))
+                    {
+                        //名前が登録されていない場合、アドレスのみ表示
+                        InternalDomainListBox.Items.Add(recipientType + smtpAddress);
+                    }
+                    else
+                    {
+                        //名前が登録されている場合、名前+アドレスを表示
+                        InternalDomainListBox.Items.Add(recipientType + recipient.Name + " <" + smtpAddress + ">");
+                    }
+                    //社内ドメイン件数カウント
+                    internalDomainCount++;
+
+                }
+                //社外ドメイン
+                else
+                {
+                    if (recipient.Name.Equals(smtpAddress))
+                    {
+                        ExternalDomainCheckBox.Items.Add(recipientType + smtpAddress);
+                    }
+                    else
+                    {
+                        ExternalDomainCheckBox.Items.Add(recipientType + recipient.Name + " <" + smtpAddress + ">");
+                    }
+
+                    //社外ドメイン件数カウント
+                    externalDomainCount++;
+                }
+
+
+            }
+
+            SendAdressNumberLabel.Text      += recipients.Count         + "件";
+            InternalDomainNumberLabel.Text  += internalDomainCount      + "件";
+            ExternalDomainNumberLabel.Text  += externalDomainCount      + "件";
+
+        }
+
+        /// <summary>
+        /// 添付ファイルの表示
+        /// </summary>
+        /// <param name="item">送信メールのアイテム</param>
+        private void AttachmentCheck(object item)
+        {
+            Outlook._MailItem mailItem = item as Outlook._MailItem;
+
+            foreach (Outlook.Attachment attachment in mailItem.Attachments)
+            {
+                AttachmentCheckBox.Items.Add(attachment.FileName);
+            }
+
+            AttachmentNumberLabel.Text += mailItem.Attachments.Count+"件";
+        }
+
+        /// <summary>
+        /// 送信ボタンの押下時処理
+        /// </summary>
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            SendButton.DialogResult = DialogResult.OK;
+        }
+
+        /// <summary>
+        /// キャンセルボタンの押下時処理
+        /// </summary>
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            CancelButton.DialogResult = DialogResult.Cancel;
+        }
+
+        /// <summary>
+        /// 社外ドメインチェックボックスのチェック時処理
+        /// </summary>
+        private void ExternalDomainCheckBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            //チェックボックスがチェックされた場合
+            if(e.NewValue == CheckState.Checked)
+            {
+                //チェックされたことによってすべてチェックがついた場合
+                if (ExternalDomainCheckBox.Items.Count == ExternalDomainCheckBox.CheckedItems.Count + 1)
+                {
+                    isExtDomainBulkCheckEvent           = false;
+                    ExternalDomainBulkCheckBox.Checked  = true;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            //チェックが外された場合
+            else
+            {
+                //チェックがはずれされる前に全てのチェックがついていた場合
+                if (ExternalDomainCheckBox.Items.Count == ExternalDomainCheckBox.CheckedItems.Count )
+                {
+                    isExtDomainBulkCheckEvent           = false;
+                    ExternalDomainBulkCheckBox.Checked  = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// 社外ドメイン一括チェック
+        /// </summary>
+        private void ExternalDomainBulkCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            bool isChecked = ExternalDomainBulkCheckBox.Checked;
+
+            if (isExtDomainBulkCheckEvent)
+            {
+                for (int i = 0; i < ExternalDomainCheckBox.Items.Count; i++)
+                {
+                    ExternalDomainCheckBox.SetItemChecked(i, isChecked);
+                }
+            }
+            else
+            {
+                //イベント発生無
+            }
+
+            isExtDomainBulkCheckEvent = true;
+            //送信ボタン活性化チェック
+            SendButtonEnableCheck();
+        }
+
+        /// <summary>
+        /// 添付ファイルチェックボックスのチェック時処理
+        /// </summary>
+        private void AttachmentCheckBox_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+            if (e.NewValue == CheckState.Checked)
+            {
+
+                if (AttachmentCheckBox.Items.Count == AttachmentCheckBox.CheckedItems.Count + 1)
+                {
+                    isAttachmentBulkCheckEvent     = false;
+                    AttachmentBulkCheckBox.Checked = true;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                if (AttachmentCheckBox.Items.Count == AttachmentCheckBox.CheckedItems.Count)
+                {
+                    isAttachmentBulkCheckEvent     = false;
+                    AttachmentBulkCheckBox.Checked = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 添付ファイル一括チェック
+        /// </summary>
+        private void AttachmentBulkCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            bool isChecked = AttachmentBulkCheckBox.Checked;
+
+            if (isAttachmentBulkCheckEvent)
+            {
+                for (int i = 0; i < AttachmentCheckBox.Items.Count; i++)
+                {
+                    AttachmentCheckBox.SetItemChecked(i, isChecked);
+                }
+
+            }
+            else
+            {
+                //イベント発生無
+            }
+
+            isAttachmentBulkCheckEvent = true;
+            //送信ボタン活性化チェック
+            SendButtonEnableCheck();
+        }
+
+
+        /// <summary>
+        /// 送信ボタンの活性、非活性チェック
+        /// </summary>
+        private void SendButtonEnableCheck()
+        {
+            //チェックが全て行われていた場合、送信ボタンを活性化
+            if (((ExternalDomainBulkCheckBox.Checked    == true) && (AttachmentBulkCheckBox.Checked     == true))
+                || ((ExternalDomainCheckBox.Items.Count == 0)    && (AttachmentCheckBox.Items.Count     == 0))
+                || ((ExternalDomainBulkCheckBox.Checked == true) && (AttachmentCheckBox.Items.Count     == 0))
+                || ((AttachmentBulkCheckBox.Checked     == true) && (ExternalDomainCheckBox.Items.Count == 0)))
+            {
+                SendButton.Enabled = true;
+            }
+            else
+            {
+                SendButton.Enabled = false;
+            }
+
+        }
+
+    }
+}
---
+++ MailYoujin/MainForm.designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MailYoujin
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendAdressGroupBox = new System.Windows.Forms.GroupBox();
+            this.ExternalDomainNumberLabel = new System.Windows.Forms.Label();
+            this.SendAdressNumberLabel = new System.Windows.Forms.Label();
+            this.InternalDomainNumberLabel = new System.Windows.Forms.Label();
+            this.InternalDomainListBox = new System.Windows.Forms.ListBox();
+            this.ExternalDomainBulkCheckBox = new System.Windows.Forms.CheckBox();
+            this.ExternalDomainLabel = new System.Windows.Forms.Label();
+            this.InternalDomainLabel = new System.Windows.Forms.Label();
+            this.ExternalDomainCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.AttachmentGroupBox = new System.Windows.Forms.GroupBox();
+            this.AttachmentNumberLabel = new System.Windows.Forms.Label();
+            this.AttachmentBulkCheckBox = new System.Windows.Forms.CheckBox();
+            this.AttachmentCheckBox = new System.Windows.Forms.CheckedListBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SendAdressGroupBox.SuspendLayout();
+            this.AttachmentGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SendAdressGroupBox
+            // 
+            this.SendAdressGroupBox.Controls.Add(this.ExternalDomainNumberLabel);
+            this.SendAdressGroupBox.Controls.Add(this.SendAdressNumberLabel);
+            this.SendAdressGroupBox.Controls.Add(this.InternalDomainNumberLabel);
+            this.SendAdressGroupBox.Controls.Add(this.InternalDomainListBox);
+            this.SendAdressGroupBox.Controls.Add(this.ExternalDomainBulkCheckBox);
+            this.SendAdressGroupBox.Controls.Add(this.ExternalDomainLabel);
+            this.SendAdressGroupBox.Controls.Add(this.InternalDomainLabel);
+            this.SendAdressGroupBox.Controls.Add(this.ExternalDomainCheckBox);
+            this.SendAdressGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.SendAdressGroupBox.Name = "SendAdressGroupBox";
+            this.SendAdressGroupBox.Size = new System.Drawing.Size(511, 310);
+            this.SendAdressGroupBox.TabIndex = 0;
+            this.SendAdressGroupBox.TabStop = false;
+            this.SendAdressGroupBox.Text = "送信アドレス";
+            // 
+            // ExternalDomainNumberLabel
+            // 
+            this.ExternalDomainNumberLabel.AutoSize = true;
+            this.ExternalDomainNumberLabel.Location = new System.Drawing.Point(76, 155);
+            this.ExternalDomainNumberLabel.Name = "ExternalDomainNumberLabel";
+            this.ExternalDomainNumberLabel.Size = new System.Drawing.Size(35, 12);
+            this.ExternalDomainNumberLabel.TabIndex = 9;
+            this.ExternalDomainNumberLabel.Text = "件数：";
+            // 
+            // SendAdressNumberLabel
+            // 
+            this.SendAdressNumberLabel.AutoSize = true;
+            this.SendAdressNumberLabel.Location = new System.Drawing.Point(76, 0);
+            this.SendAdressNumberLabel.Name = "SendAdressNumberLabel";
+            this.SendAdressNumberLabel.Size = new System.Drawing.Size(35, 12);
+            this.SendAdressNumberLabel.TabIndex = 8;
+            this.SendAdressNumberLabel.Text = "件数：";
+            // 
+            // InternalDomainNumberLabel
+            // 
+            this.InternalDomainNumberLabel.AutoSize = true;
+            this.InternalDomainNumberLabel.Location = new System.Drawing.Point(76, 20);
+            this.InternalDomainNumberLabel.Name = "InternalDomainNumberLabel";
+            this.InternalDomainNumberLabel.Size = new System.Drawing.Size(35, 12);
+            this.InternalDomainNumberLabel.TabIndex = 7;
+            this.InternalDomainNumberLabel.Text = "件数：";
+            // 
+            // InternalDomainListBox
+            // 
+            this.InternalDomainListBox.FormattingEnabled = true;
+            this.InternalDomainListBox.ItemHeight = 12;
+            this.InternalDomainListBox.Location = new System.Drawing.Point(10, 39);
+            this.InternalDomainListBox.Name = "InternalDomainListBox";
+            this.InternalDomainListBox.Size = new System.Drawing.Size(490, 100);
+            this.InternalDomainListBox.TabIndex = 6;
+            // 
+            // ExternalDomainBulkCheckBox
+            // 
+            this.ExternalDomainBulkCheckBox.AutoSize = true;
+            this.ExternalDomainBulkCheckBox.Location = new System.Drawing.Point(13, 172);
+            this.ExternalDomainBulkCheckBox.Name = "ExternalDomainBulkCheckBox";
+            this.ExternalDomainBulkCheckBox.Size = new System.Drawing.Size(79, 16);
+            this.ExternalDomainBulkCheckBox.TabIndex = 5;
+            this.ExternalDomainBulkCheckBox.Text = "一括チェック";
+            this.ExternalDomainBulkCheckBox.UseVisualStyleBackColor = true;
+            this.ExternalDomainBulkCheckBox.CheckedChanged += new System.EventHandler(this.ExternalDomainBulkCheckBox_CheckedChanged);
+            // 
+            // ExternalDomainLabel
+            // 
+            this.ExternalDomainLabel.AutoSize = true;
+            this.ExternalDomainLabel.Location = new System.Drawing.Point(10, 155);
+            this.ExternalDomainLabel.Name = "ExternalDomainLabel";
+            this.ExternalDomainLabel.Size = new System.Drawing.Size(64, 12);
+            this.ExternalDomainLabel.TabIndex = 4;
+            this.ExternalDomainLabel.Text = "社外ドメイン";
+            // 
+            // InternalDomainLabel
+            // 
+            this.InternalDomainLabel.AutoSize = true;
+            this.InternalDomainLabel.Location = new System.Drawing.Point(10, 20);
+            this.InternalDomainLabel.Name = "InternalDomainLabel";
+            this.InternalDomainLabel.Size = new System.Drawing.Size(64, 12);
+            this.InternalDomainLabel.TabIndex = 3;
+            this.InternalDomainLabel.Text = "社内ドメイン";
+            // 
+            // ExternalDomainCheckBox
+            // 
+            this.ExternalDomainCheckBox.CheckOnClick = true;
+            this.ExternalDomainCheckBox.ForeColor = System.Drawing.Color.Red;
+            this.ExternalDomainCheckBox.FormattingEnabled = true;
+            this.ExternalDomainCheckBox.Location = new System.Drawing.Point(10, 193);
+            this.ExternalDomainCheckBox.Name = "ExternalDomainCheckBox";
+            this.ExternalDomainCheckBox.Size = new System.Drawing.Size(490, 102);
+            this.ExternalDomainCheckBox.TabIndex = 1;
+            this.ExternalDomainCheckBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.ExternalDomainCheckBox_ItemCheck);
+            // 
+            // AttachmentGroupBox
+            // 
+            this.AttachmentGroupBox.Controls.Add(this.AttachmentNumberLabel);
+            this.AttachmentGroupBox.Controls.Add(this.AttachmentBulkCheckBox);
+            this.AttachmentGroupBox.Controls.Add(this.AttachmentCheckBox);
+            this.AttachmentGroupBox.Location = new System.Drawing.Point(12, 328);
+            this.AttachmentGroupBox.Name = "AttachmentGroupBox";
+            this.AttachmentGroupBox.Size = new System.Drawing.Size(510, 156);
+            this.AttachmentGroupBox.TabIndex = 1;
+            this.AttachmentGroupBox.TabStop = false;
+            this.AttachmentGroupBox.Text = "添付ファイル";
+            // 
+            // AttachmentNumberLabel
+            // 
+            this.AttachmentNumberLabel.AutoSize = true;
+            this.AttachmentNumberLabel.Location = new System.Drawing.Point(76, 0);
+            this.AttachmentNumberLabel.Name = "AttachmentNumberLabel";
+            this.AttachmentNumberLabel.Size = new System.Drawing.Size(35, 12);
+            this.AttachmentNumberLabel.TabIndex = 10;
+            this.AttachmentNumberLabel.Text = "件数：";
+            // 
+            // AttachmentBulkCheckBox
+            // 
+            this.AttachmentBulkCheckBox.AutoSize = true;
+            this.AttachmentBulkCheckBox.Location = new System.Drawing.Point(13, 23);
+            this.AttachmentBulkCheckBox.Name = "AttachmentBulkCheckBox";
+            this.AttachmentBulkCheckBox.Size = new System.Drawing.Size(79, 16);
+            this.AttachmentBulkCheckBox.TabIndex = 4;
+            this.AttachmentBulkCheckBox.Text = "一括チェック";
+            this.AttachmentBulkCheckBox.UseVisualStyleBackColor = true;
+            this.AttachmentBulkCheckBox.CheckedChanged += new System.EventHandler(this.AttachmentBulkCheckBox_CheckedChanged);
+            // 
+            // AttachmentCheckBox
+            // 
+            this.AttachmentCheckBox.CheckOnClick = true;
+            this.AttachmentCheckBox.FormattingEnabled = true;
+            this.AttachmentCheckBox.Location = new System.Drawing.Point(10, 45);
+            this.AttachmentCheckBox.Name = "AttachmentCheckBox";
+            this.AttachmentCheckBox.Size = new System.Drawing.Size(490, 102);
+            this.AttachmentCheckBox.TabIndex = 3;
+            this.AttachmentCheckBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.AttachmentCheckBox_ItemCheck);
+            // 
+            // SendButton
+            // 
+            this.SendButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.SendButton.Enabled = false;
+            this.SendButton.Location = new System.Drawing.Point(357, 490);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "送信";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Location = new System.Drawing.Point(447, 490);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 3;
+            this.CancelButton.Text = "キャンセル";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(542, 526);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.AttachmentGroupBox);
+            this.Controls.Add(this.SendAdressGroupBox);
+            this.Name = "MainForm";
+            this.Text = "MailChecker";
+            this.SendAdressGroupBox.ResumeLayout(false);
+            this.SendAdressGroupBox.PerformLayout();
+            this.AttachmentGroupBox.ResumeLayout(false);
+            this.AttachmentGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox SendAdressGroupBox;
+        private System.Windows.Forms.GroupBox AttachmentGroupBox;
+        private System.Windows.Forms.Label ExternalDomainLabel;
+        private System.Windows.Forms.Label InternalDomainLabel;
+        private System.Windows.Forms.CheckBox ExternalDomainBulkCheckBox;
+        private System.Windows.Forms.CheckBox AttachmentBulkCheckBox;
+        private System.Windows.Forms.ListBox InternalDomainListBox;
+        private System.Windows.Forms.CheckedListBox ExternalDomainCheckBox;
+        private System.Windows.Forms.CheckedListBox AttachmentCheckBox;
+        private System.Windows.Forms.Button SendButton;
+        private new System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label ExternalDomainNumberLabel;
+        private System.Windows.Forms.Label SendAdressNumberLabel;
+        private System.Windows.Forms.Label InternalDomainNumberLabel;
+        private System.Windows.Forms.Label AttachmentNumberLabel;
+    }
+}
